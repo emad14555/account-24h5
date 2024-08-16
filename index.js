e7c0cae3170fb0426036f3a099f03e1bc573c5f7
emad14555 <su324614@gmail.com>
2024-08-17 01:30:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const { Client } = require('discord.js-selfbot-v13');
-const client = new Client(); 
-
-client.on('ready', async () => {
-  console.log(`${client.user.username} is ready!`);
-})
-//ثبات فويس 24 ساعه v13 بدون اي مشاكل
 const { joinVoiceChannel } = require('@discordjs/voice');
-client.on('ready', () => {
-    
-    setInterval( async () => {
-    client.channels.fetch(process.env.channel) 
-     .then((channel) => { 
-      const VoiceConnection = joinVoiceChannel({
-       channelId: channel.id, 
-       guildId: process.env.guild, 
-       selfMute: true,
-       selfDeaf: true,
-       adapterCreator: channel.guild.voiceAdapterCreator 
-       });
-    }).catch((error) => { return; });
-    }, 1000)
-}); 
 const express = require('express');
 const app = express();
+const client = new Client();
 const port = process.env.PORT || 3000;
 
+// Start Express server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+
+  setInterval(async () => {
+    try {
+      const channel = await client.channels.fetch(process.env.channel);
+      joinVoiceChannel({
+        channelId: channel.id,
+        guildId: process.env.guild,
+        selfMute: true,
+        selfDeaf: true,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+      });
+      console.log(`Connected to voice channel: ${channel.id}`);
+    } catch (error) {
+      console.error(`Error connecting to voice channel: ${error}`);
+    }
+  }, 10000); // Set interval to 10 seconds
+});
 //https://ra3dstudio.com CopyRight Codes
-client.login(process.env.token);
+client.login(process.env.TOKEN);
