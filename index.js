5060edabba907891e026c75cb7472e98aadbc2df
emad14555 <su324614@gmail.com>
2024-08-21 06:01:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client(); 
+//environment
+require('dotenv').config()
 
+function formatTime() { 
+  const date = new Date();
+  const options = {
+    timeZone: 'America/New_York', 
+    hour12: true,
+    hour: 'numeric',
+    minute: 'numeric'
+  };
+  return new Intl.DateTimeFormat('en-US', options).format(date);
+}
+const express = require("express")
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get('/', (req, res) => {
+  res.send(`
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>`)
+});
 client.on('ready', async () => {
   console.log(`${client.user.username} is ready!`);
 })
